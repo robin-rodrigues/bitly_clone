b4aff14ad8ac2176c893f658e3a906259d4cc198
robin-rodrigues <robinrodrigues6499@gmail.com>
2025-02-13 21:50:47 +0530

    initial backend code

---
+++ backend/src/main/java/com/bitly/clone/BitlyCloneApplication.java
@@ -0,0 +1,13 @@
+package com.bitly.clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitlyCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitlyCloneApplication.class, args);
+	}
+
+}
