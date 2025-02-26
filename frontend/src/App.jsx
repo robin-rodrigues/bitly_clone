ead382332b1b5434c5dfcadda5621e0fb4aef217
robin-rodrigues <robinrodrigues6499@gmail.com>
2025-02-26 14:04:39 +0530

    initial frontend react code

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 class="text-5xl font-bold underline">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
