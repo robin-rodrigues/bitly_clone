ff672f76fe39b907d446dc807d8f04c6b21f153a
robin-rodrigues <robinrodrigues6499@gmail.com>
2025-02-26 20:53:56 +0530

    Added landing page and signup/login functionality in React

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,27 @@
-import './App.css'
+import "./App.css";
+import Footer from "./components/Footer";
+import LandingPage from "./components/LandingPage";
+import AboutPage from "./components/AboutPage";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Navbar from "./components/NavBar";
+import RegisterPage from "./components/RegisterPage";
+import { Toaster } from "react-hot-toast";
+import LoginPage from "./components/LoginPage";
 
 function App() {
-  return (
-    <>
-      <h1 class="text-5xl font-bold underline">
-        Hello world!
-      </h1>
-    </>
-  )
+    return (
+        <Router>
+            <Navbar />
+            <Toaster position="bottom-center" />
+            <Routes>
+                <Route path="/" element={<LandingPage />} />
+                <Route path="/about" element={<AboutPage />} />
+                <Route path="/register" element={<RegisterPage />} />
+                <Route path="/login" element={<LoginPage />} />
+            </Routes>
+            <Footer />
+        </Router>
+    );
 }
 
-export default App
+export default App;
