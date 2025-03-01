7265bbf10b08c1d70142072848ae3d2d47a486f5
robin-rodrigues <robinrodrigues6499@gmail.com>
2025-03-01 17:07:25 +0530

    added private routes to block dashboards for unauthenticated users

--- frontend/src/AppRouter.jsx
+++ frontend/src/AppRouter.jsx
@@ -8,6 +8,7 @@ import { Toaster } from "react-hot-toast";
 import LoginPage from "./components/LoginPage";
 import DashboardLayout from "./components/dashboard/DashboardLayout";
 import ShortenUrlPage from "./components/ShortenUrlPage";
+import PrivateRoute from "./PrivateRoute";
 
 const AppRouter = () => {
     return (
@@ -17,9 +18,30 @@ const AppRouter = () => {
             <Routes>
                 <Route path="/" element={<LandingPage />} />
                 <Route path="/about" element={<AboutPage />} />
-                <Route path="/register" element={<RegisterPage />} />
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/dashboard" element={<DashboardLayout />} />
+                <Route
+                    path="/register"
+                    element={
+                        <PrivateRoute publicPage={true}>
+                            <RegisterPage />
+                        </PrivateRoute>
+                    }
+                />
+                <Route
+                    path="/login"
+                    element={
+                        <PrivateRoute publicPage={true}>
+                            <LoginPage />
+                        </PrivateRoute>
+                    }
+                />
+                <Route
+                    path="/dashboard"
+                    element={
+                        <PrivateRoute publicPage={false}>
+                            <DashboardLayout />
+                        </PrivateRoute>
+                    }
+                />
             </Routes>
             <Footer />
         </>
