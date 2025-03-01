a2bbd080c307fd1fd0feb46aa0880ab85c6525f0
robin-rodrigues <robinrodrigues6499@gmail.com>
2025-03-01 17:18:19 +0530

    updated navbar to reflect different buttons for login and logout cases

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -2,12 +2,19 @@ import React, { useState } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { IoIosMenu } from "react-icons/io";
 import { RxCross2 } from "react-icons/rx";
+import { useStoreContext } from "../contextApi/ContextApi";
 
 const Navbar = () => {
+    const navigate = useNavigate();
+    const { token, setToken } = useStoreContext();
     const path = useLocation().pathname;
     const [navbarOpen, setNavbarOpen] = useState(false);
 
-    const onLogOutHandler = () => {};
+    const onLogOutHandler = () => {
+        setToken(null);
+        localStorage.removeItem("JWT_TOKEN");
+        navigate("/login");
+    };
 
     return (
         <div className="h-16 bg-custom-gradient  z-50 flex items-center sticky top-0 ">
@@ -36,6 +43,7 @@ const Navbar = () => {
                             Home
                         </Link>
                     </li>
+
                     <li className="hover:text-btnColor font-[500]  transition-all duration-150">
                         <Link
                             className={`${
@@ -48,11 +56,38 @@ const Navbar = () => {
                             About
                         </Link>
                     </li>
-                    <Link to="/register">
-                        <li className=" sm:ml-0 -ml-1 bg-rose-700 text-white  cursor-pointer w-24 text-center font-semibold px-2 py-2 rounded-md  hover:text-slate-300   transition-all duration-150">
-                            SignUp
+
+                    {token && (
+                        <li className="hover:text-btnColor font-[500]  transition-all duration-150">
+                            <Link
+                                className={`${
+                                    path === "/dashboard"
+                                        ? "text-white font-semibold"
+                                        : "text-gray-200"
+                                }`}
+                                to="/dashboard"
+                            >
+                                Dashboard
+                            </Link>
                         </li>
-                    </Link>
+                    )}
+
+                    {!token && (
+                        <Link to="/register">
+                            <li className=" sm:ml-0 -ml-1 bg-rose-700 text-white  cursor-pointer w-24 text-center font-semibold px-2 py-2 rounded-md  hover:text-slate-300   transition-all duration-150">
+                                SignUp
+                            </li>
+                        </Link>
+                    )}
+
+                    {token && (
+                        <button
+                            onClick={onLogOutHandler}
+                            className="sm:ml-0 -ml-1 bg-rose-700 text-white  cursor-pointer w-24 text-center font-semibold px-2 py-2 rounded-md  hover:text-slate-300   transition-all duration-150"
+                        >
+                            LogOut
+                        </button>
+                    )}
                 </ul>
                 <button
                     onClick={() => setNavbarOpen(!navbarOpen)}
