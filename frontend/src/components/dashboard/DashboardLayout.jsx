0adb5a5c47dd753ca0a874d48c48dc51a83125ba
robin-rodrigues <robinrodrigues6499@gmail.com>
2025-03-01 12:44:50 +0530

    Added functionality to create new short urls

--- frontend/src/components/dashboard/DashboardLayout.jsx
+++ frontend/src/components/dashboard/DashboardLayout.jsx
@@ -1,23 +1,30 @@
 import React, { useState } from "react";
 import Graph from "./Graph";
+import { dummyData } from "../../dummyData/data";
 import { useStoreContext } from "../../contextApi/ContextApi";
 import { useFetchTotalClicks } from "../../hooks/useQuery";
+import ShortenPopUp from "./ShortenPopUp";
 
 const DashboardLayout = () => {
+    const refetch = false;
     const { token } = useStoreContext();
+    const [shortenPopUp, setShortenPopUp] = useState(false);
+
+    // console.log(useFetchTotalClicks(token, onError));
+
     const { isLoading: loader, data: totalClicks } = useFetchTotalClicks(
         token,
         onError
     );
 
     function onError() {
-        navigate("/error");
+        console.log("ERROR");
     }
 
     return (
         <div className="lg:px-14 sm:px-8 px-4 min-h-[calc(100vh-64px)]">
             {loader ? (
-                <p>Loading...</p>
+                <p>Loading....</p>
             ) : (
                 <div className="lg:w-[90%] w-full mx-auto py-16">
                     <div className=" h-96 relative ">
@@ -35,12 +42,21 @@ const DashboardLayout = () => {
                         <Graph graphData={totalClicks} />
                     </div>
                     <div className="py-5 sm:text-end text-center">
-                        <button className="bg-custom-gradient px-4 py-2 rounded-md text-white">
+                        <button
+                            className="bg-custom-gradient px-4 py-2 rounded-md text-white"
+                            onClick={() => setShortenPopUp(true)}
+                        >
                             Create a New Short URL
                         </button>
                     </div>
                 </div>
             )}
+
+            <ShortenPopUp
+                refetch={refetch}
+                open={shortenPopUp}
+                setOpen={setShortenPopUp}
+            />
         </div>
     );
 };
