774166dd1cc99488154cf410ba3586b522787902
robin-rodrigues <robinrodrigues6499@gmail.com>
2025-03-11 21:14:25 +0530

    fixed the redirection logic for short urls

--- frontend/src/utils/helper.js
+++ frontend/src/utils/helper.js
@@ -1,13 +1,18 @@
 import { subDomainList } from "./constant";
+import { SubDomainRouter } from "../AppRouter";
 
 export const getApps = () => {
     const subdomain = getSubDomain(window.location.hostname);
-
     const mainApp = subDomainList.find((app) => app.main);
+
+    // Check if the path matches a shortened URL (e.g., '/eDaqQ0Vd')
+    const isShortenedUrl = /^\/[a-zA-Z0-9]+$/.test(window.location.pathname);
+
+    if (isShortenedUrl) return SubDomainRouter;
+
     if (subdomain === "") return mainApp.app;
 
     const apps = subDomainList.find((app) => subdomain === app.subdomain);
-
     return apps ? apps.app : mainApp.app;
 };
 
